42d87087a3b05e80e75274344ef5c73639f56f6d
Unchittha00 <s6303051633085@email.kmutnb.ac.th>
2023-09-29 20:32:57 +0700

    text-check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
-import Router3 from "./router3";
+import React from "react";
+import FormCheck from "./form-check";
+// import FormText from "./form-text";
+// import Router2 from "./router2";
+// import Router3 from "./router3";
+// import Banner from "./banner";
+// import { Calculator2 } from "./calculator";
+// import { EventData2 } from "./event-data";
+// import { Header,Content,Footer } from "./func-components";
+// import RefsFunc from "./refs-func";  
+// import Calendar from "./class-components";
+// import RefsArray from "./refs-array";
+// import MessageBox from "./state-func";
+// import { userContext } from './context';
+// import Header from "./context-header";
+// import Content from "./context-content";
+// import Header2 from "./context-header2";
+// import Content2 from "./context-content2";
 
-export default function App() {
-  return <Router3/>
+function App() {
+  // let [user,setUser] = React.useState()
+  return (
+    <>
+      {/* <Header/>
+      <p><center><Calendar/></center></p>
+      <Content/>
+      <Banner/>
+      <Calculator2/>
+      <EventData2/>
+      <RefsFunc/> */}
+      {/* <RefsArray/> */}
+      {/* <RefsArray/> */}
+      {/* <Footer/> */}
+      {/* <MessageBox/> */}
+      {/* <userContext.Provider value={'Tom jerry'}>
+        <Header/>
+        <Content/>
+      </userContext.Provider> */}
+      
+      {/* <userContext.Provider value={[user,setUser]}>
+        <Header2/>
+        <Content2/>
+      </userContext.Provider> */}
+      {/* <Router3/> */}
+      {/* <FormText/> */}
+      <FormCheck/>
+    </>
+  )
 }
+
+export default App
